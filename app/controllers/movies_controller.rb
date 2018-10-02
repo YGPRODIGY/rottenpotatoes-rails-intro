7f3b310ec3e556d03dc311dbcdebdc42dec6dd0b
EC2 Default User <ec2-user@ip-172-31-2-96.us-east-2.compute.internal>
2018-10-02 02:09:39 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,26 +11,47 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    
     @all_ratings = Movie.all_ratings
-    @selected_ratings = params[:ratings]
-    if @selected_ratings == nil
-      @selected_ratings = {'G' => '1','PG' => '1','PG-13' => '1','R' => '1'}
-    else
-      @movies = Movie.where(:rating => @selected_ratings.keys)
-    end
+  
     sort_choice = params[:sort]
+   
+    if params[:ratings] == nil
+      if session[:ratings] == nil
+        @selected_ratings = @all_ratings
+        session[:ratings] = @selected_ratings
+      else
+        @selected_ratings = session[:ratings]
+      end
+    else
+      @selected_ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    end
+    
+    if params[:sort] == nil
+      if session[:sort] == nil
+        sort_choice = Movie.all
+        session[:sort] = sort_choice
+      else
+        sort_choice = session[:sort]
+      end
+    else
+      sort_choice = params[:sort]
+      session[:sort] = params[:sort]
+    end
+    
+  
+    
     if sort_choice == 'title'
       @title_header = 'hilite'
-      @movies = Movie.all.order(:title)
-
-    end
-    if sort_choice == 'release_date'
-      @release_header = 'hilite'
-      @movies = Movie.all.order(:release_date)
-
+      @movies =  Movie.where(:rating => @selected_ratings.keys).order(title: :asc)
+    elsif sort_choice == 'release_date'
+        @release_header = 'hilite'
+        @movies =  Movie.where(:rating => @selected_ratings.keys).order(release_date: :asc)
     end
   end
+
+  
   def new
     # default: render 'new' template
   end
